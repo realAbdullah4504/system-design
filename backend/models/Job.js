788e6c4ee1d93f3ca75e714bc8b51f3d6e6404d2
realAbdullah4504 <abdullahjaved4504@gmail.com>
2026-02-02 13:45:10 +0500

    module type added

--- backend/models/Job.js
+++ backend/models/Job.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-const { v4: uuidv4 } = require("uuid");
+import mongoose from "mongoose";
+import { v4 as uuidv4 } from "uuid";
 
 const jobSchema = new mongoose.Schema(
   {
@@ -26,4 +26,4 @@ const jobSchema = new mongoose.Schema(
 // Compound index for idempotency
 jobSchema.index({ messageId: 1 }, { unique: true });
 
-module.exports = mongoose.model("Job", jobSchema);
+export default mongoose.model("Job", jobSchema);
