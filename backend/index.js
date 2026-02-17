9988ebda56f4dd53868ee52c4b01015112c95a1f
realAbdullah4504 <abdullahjaved4504@gmail.com>
2026-02-17 12:28:31 +0500

    done the sqs integration

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import os from "os";
 import { sendMessage } from "./services/sqs.js";
+import { pollQueue } from "./worker.js";
 const app = express();
 app.use(express.json());
 
@@ -54,4 +55,5 @@ app.get("/health", (req, res) => {
 
 app.listen(3000, () => {
   console.log("API running on port 3000");
+  pollQueue();
 });
