2328f024463c5e933b183336399aff480528ee6d
realAbdullah4504 <abdullahjaved4504@gmail.com>
2026-03-01 14:40:54 +0500

    integration tests

--- backend/config/sns.js
+++ backend/config/sns.js
@@ -5,15 +5,15 @@ console.log(process.env.AWS_ACCESS_KEY_ID)
 console.log(process.env.AWS_SECRET_ACCESS_KEY)
 console.log(process.env.AWS_REGION)
 console.log(process.env.TOPIC_ARN)
-if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY || !process.env.AWS_REGION || !process.env.TOPIC_ARN) {
+if (!process.env.AWS_REGION || !process.env.TOPIC_ARN) {
     throw new Error("AWS credentials not found");
 }
 export const snsClient = new SNSClient({
     region: process.env.AWS_REGION,
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    }
+    // credentials: {
+    //     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    //     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    // }
 });
 
 export const TOPIC_ARN = process.env.TOPIC_ARN;
