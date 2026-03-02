0e00576a485ba01168db6733c3b27de5379f26e0
realAbdullah4504 <abdullahjaved4504@gmail.com>
2026-03-02 15:18:22 +0500

    prom tail loki logs added

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
+  if (req.path === '/metrics') return next();
   logger.info('HTTP request', {
     method: req.method,
     url: req.url,
@@ -26,6 +27,7 @@ app.use((req, res, next) => {
 
 // Prometheus metrics middleware
 app.use((req, res, next) => {
+  if (req.path === '/metrics') return next();
   const start = Date.now();
   
   res.on('finish', () => {
